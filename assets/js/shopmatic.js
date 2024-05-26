28e4308a6c5ea6b1768ce5220476502e322723e3
DESKTOP-A8H8TTO\Fauzi <yosephramadhan19@gmail.com>
2024-05-26 19:38:45 +0700

    fix(tab): fix tab link

--- assets/js/shopmatic.js
+++ assets/js/shopmatic.js
@@ -14,4 +14,66 @@ $(document).ready(function (){
             var type = $(this).hasClass("hide") ? "text" : "password";
             $("#confirm-password").attr("type", type);
     });
+
+    // open forgot tab
+
+    $('.open-forgot').on('click', function (e) {
+        e.preventDefault();
+        $('#default-tab-content > div').addClass('hidden');
+        $('#forgot').removeClass('hidden');
+
+        var targetTab = $('#heading-tab > li > button[aria-selected="true"]');
+            targetTab.html('Forgot Password');
+    });
+
+    $('#login-tab').on('click', function (e) {
+        e.preventDefault();
+        $('#forgot').addClass('hidden');
+        $(this).html('Login');
+        $('#register-tab').html('Register');
+
+        $('#register-tab').attr('aria-selected', false);
+        $('#default-tab-content > div').addClass('hidden');
+
+        $('#login').removeClass('hidden');
+    });
+
+    $('#register-tab').on('click', function (e) {
+        e.preventDefault();
+        $('#forgot').addClass('hidden');
+
+        $(this).html('Register');
+        $('#login-tab').html('Login');
+
+        $('#login-tab').attr('aria-selected', false);
+        $('#default-tab-content > div').addClass('hidden');
+
+        $('#register').removeClass('hidden');
+    });
+
+    $('.open-register').on('click', function (e) {
+        e.preventDefault();
+        $('#default-tab-content > div').addClass('hidden');
+        $('#register').removeClass('hidden');
+
+        $('#register-tab').attr('aria-selected', true);
+
+        $('#heading-tab > li > button').removeClass('bg-shopmatic-primary');
+        $('#heading-tab > li > button').removeClass('text-white');
+
+        $('#heading-tab > li > button').addClass(' text-black');        
+        $('#register-tab').addClass('bg-shopmatic-primary text-white hover:text-white rounded-t-lg');
+    });
+
+    $('.open-login').on('click', function (e) {
+        e.preventDefault();
+        $('#default-tab-content > div').addClass('hidden');
+        $('#login').removeClass('hidden');
+        $('#login-tab').attr('aria-selected', true);
+
+        $('#heading-tab > li > button').removeClass('bg-shopmatic-primary');
+        $('#heading-tab > li > button').removeClass('text-white');
+        $('#heading-tab > li > button').addClass(' text-black');
+        $('#login-tab').addClass('bg-shopmatic-primary text-white hover:text-white rounded-t-lg');
+    });
 });
